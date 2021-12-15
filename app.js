45d41539990c8e9c1bd6fc9c615dcbb01d4f7c04
Sadiq Shah <52124323+sadiqshah786@users.noreply.github.com>
2021-12-15 14:31:45 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,79 @@
+let auth = firebase.auth();
+var email = document.getElementById('email');
+var pass = document.getElementById('password');
+
+function signup(){
+  var email = document.getElementById('email');
+  var pass = document.getElementById('password');
+  if(email.value==""){
+    swal({
+      title: "Empty Email Feild",
+    });
+  }
+  else if(pass.value==""){
+    swal({
+      title: "Empty Password  Feild",
+    });
+    
+  }
+  else{
+    auth.createUserWithEmailAndPassword(email.value, pass.value)
+    .then((userCredential) => {
+      // Signed in 
+      var user = userCredential.user;
+      swal("Registerd Sucessfully", "You clicked the button!", "success", {
+        button: "Can LogIn",
+      });
+      // swal("Registerd Sucessfully !")
+      // window.location.replace("signIn.html")
+      
+  
+      // ...
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      alert(errorMessage)
+      
+    });
+  
+    }
+
+  }
+
+
+  function login(){
+    var email = document.getElementById('email-login');
+    var pass = document.getElementById('password-login');
+    if(email.value==""){
+      swal({
+        title: "Empty Email Feild",
+      });
+    }
+    else if(pass.value==""){
+      swal({
+        title: "Empty Password  Feild",
+      });
+      
+    }
+    else{
+    firebase.auth().signInWithEmailAndPassword(email.value, pass.value)
+    .then((userCredential) => {
+      // Signed in
+      var user = userCredential.user;
+      window.location.replace('https://github.com/sadiqshah786/Fusion-Creative-Website')
+      // console.log(user)
+      // swal(user)  
+      // alert("login")
+      // ...
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      alert(errorMessage)
+    });
+  }
+  }
+
+
+
